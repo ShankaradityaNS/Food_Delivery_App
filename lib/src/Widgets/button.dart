1611483cc773dd51dd9eb1f84ee14ee4a28a4d30
Shankaraditya N S <50665969+ShankaradityaNS@users.noreply.github.com>
2020-07-14 22:35:57 +0530

    Create button.dart

---
+++ lib/src/Widgets/button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String btnText;
+  Button({this.btnText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.blue,
+      ),
+      child: Center(
+        child: Text(
+          "$btnText",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.white, fontSize: 18.0),
+        ),
+      ),
+    );
+  }
+}
